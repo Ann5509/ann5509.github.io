9c1cded3c7cf35868204f985b1d5e8e574e5ee6f
Ann <ann.lin@hellowsanta.com.tw>
2019-02-10 22:33:11 +0800

    更新

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,12 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     uglifycss = require('gulp-uglifycss');
 
-gulp.task('gaf', ['js', 'scss'], function(){
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('js', function(){
-   return gulp.src('./js/script.js')
-       .pipe(concat('app.min.js'))
-       .pipe(uglify())
-       // .pipe(minify())
-       .pipe(gulp.dest('./js'))
+gulp.task('js', function () {
+    return gulp.src('./js/script.js')
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        // .pipe(minify())
+        .pipe(gulp.dest('./js'))
 });
 
 gulp.task('scss', function () {
@@ -27,3 +22,15 @@ gulp.task('scss', function () {
         // .pipe(minifyCss())
         .pipe(gulp.dest('./css'))
 });
+
+// gulp.task("default", ["js", "scss"], function() {
+//   return gulp.src("src/index.html").pipe(gulp.dest("dist"));
+// });
+
+gulp.task("watch", function() {
+  //自定一個watch的排程名稱
+    gulp.watch("./scss/style.scss", ["scss"]); //監聽路徑，以及檔案變更後所執行的任務
+    gulp.watch("./js/script.js", ["js"]); //監聽路徑，以及檔案變更後所執行的任務
+});
+
+gulp.task("default", ["scss", "js", "watch"]);
